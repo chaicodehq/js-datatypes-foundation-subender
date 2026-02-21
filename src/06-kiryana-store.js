13ba4d4c720e69feb691f0259bc459576a939bce
Subender <subender.io@outlook.com>
2026-02-21 06:09:12 +0530

    complete Kiryana store

--- src/06-kiryana-store.js
+++ src/06-kiryana-store.js
@@ -52,20 +52,50 @@
  */
 export function getItemNames(items) {
   // Your code here
+  if(!Array.isArray(items))return[];
+
+  return items.map(item=> item.name);
+
+
 }
 
 export function getAffordableItems(items, maxPrice) {
   // Your code here
+  if(!Array.isArray(items) || typeof maxPrice !== 'number')return [];
+
+  return items.filter(item => item.price <= maxPrice);
+
+ 
 }
 
 export function calculateTotal(items) {
   // Your code here
+  if(!Array.isArray(items) || items.length < 1)return 0
+
+  return items.reduce((acc, item)=>{
+    return acc + (item.price * item.qty);
+  }, 0)
 }
 
 export function sortByPrice(items, ascending) {
   // Your code here
+  if(!Array.isArray(items))return [];
+
+  if(ascending){
+
+    return [...items].sort((a,b)=> a.price - b.price)
+  }else{
+    return [...items].sort((a,b)=>b.price-a.price);
+  }
+
 }
 
 export function formatBill(items) {
   // Your code here
+  if(!Array.isArray(items) || items.length < 1)return ""
+
+  return items.map((item)=>{
+    return `${item.name} x ${item.qty} = Rs.${item.price * item.qty}`
+  }).join("\n")
+
 }
